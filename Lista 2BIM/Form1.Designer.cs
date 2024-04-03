093eb526d98279aa596900098443a627d633aa4c
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-04-03 10:30:40 -0300

    Second Exercise

---
+++ Lista 2BIM/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Lista_2BIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lista 2BIM/Form1.cs
@@ -0,0 +1,82 @@
+﻿/********************************************************************
+*Colegio Técnico Antônio Teixeira Fernandes (Univap)
+*Curso Técnico em Informática - Data de Entrega: 04 / 04 / 2024
+* Autores do Projeto: Vitor Lucas Kohls Correa
+*                     Isabelly Pacheco Marinho
+*
+* Turma: 3F
+* Atividade Proposta em aula
+********************************************************************/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lista_2BIM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void DrawLine(int x0, int y0, int x1, int y1, Pen pen, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(pen, x0, y0, x1, y1);
+        }
+        Pen PenPatterned(int r, int g, int b, float[] pattern)
+        {
+            Pen pen = new Pen(Color.FromArgb(r,g,b));
+            pen.DashPattern = pattern;
+            return pen;
+        }
+        Pen PenPatterned(int r, int g, int b)
+        {
+            Pen pen = new Pen(Color.FromArgb(r, g, b));
+            return pen;
+        }
+        void DrawRectangle(PaintEventArgs e, Pen pen, int x, int y, int l, int h)
+        {
+            e.Graphics.DrawRectangle(pen, x, y, l, h);
+        }
+        public void DrawTriangle(PaintEventArgs e, Pen pen, int x0, int y0, int x1, int y1, int x2, int y2)
+        {
+            //Computes the Euclidian distance between the coords
+            float a = (float) Math.Sqrt(Math.Pow(x0 - x1,2) + Math.Pow(y0 - y1,2));
+            float b = (float) Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2));
+            float c = (float) Math.Sqrt(Math.Pow(x2 - x0,2) + Math.Pow(y2 - y0,2));
+
+            if (a == 0 || b == 0 || c == 0)
+            {
+                MessageBox.Show("Not a Triangle.");
+                return;
+            }
+
+            if (a == b && b == c)
+                label1.Text = "Equilateral";
+            else if(a == b && b != c)
+                label1.Text = "Isoceles";
+            else
+                label1.Text = "Scalene";
+
+            DrawLine(x0, y0, x1, y1, pen, e);
+            DrawLine(x1, y1, x2, y2, pen, e);
+            DrawLine(x2, y2, x0, y0, pen, e);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            float[] pattern = {5,1,5,1};
+            Pen pen = PenPatterned(0, 0, 0, pattern);
+            DrawLine(100,100,400,400,pen,e);
+
+            DrawRectangle(e,pen, 200,100,10,10);
+
+            DrawTriangle(e,pen, 250,130, 100, 90, 160, 90);
+        }
+    }
+}
