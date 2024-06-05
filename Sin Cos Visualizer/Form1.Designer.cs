0a82d2bd74aeb102b3476e1179ce81acb3100460
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-06-05 10:39:47 -0300

    visualizador de função trigonométricas

---
+++ Sin Cos Visualizer/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Sin_Cos_Visualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.amplitudeTextBox = new System.Windows.Forms.TextBox();
+            this.frequenciaTextBox = new System.Windows.Forms.TextBox();
+            this.desenharButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.formula = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sin",
+            "Cos"});
+            this.comboBox1.Location = new System.Drawing.Point(65, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 33);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Sin";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // amplitudeTextBox
+            // 
+            this.amplitudeTextBox.Location = new System.Drawing.Point(24, 82);
+            this.amplitudeTextBox.Name = "amplitudeTextBox";
+            this.amplitudeTextBox.Size = new System.Drawing.Size(57, 26);
+            this.amplitudeTextBox.TabIndex = 1;
+            this.amplitudeTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // frequenciaTextBox
+            // 
+            this.frequenciaTextBox.Location = new System.Drawing.Point(123, 82);
+            this.frequenciaTextBox.Name = "frequenciaTextBox";
+            this.frequenciaTextBox.Size = new System.Drawing.Size(57, 26);
+            this.frequenciaTextBox.TabIndex = 2;
+            // 
+            // desenharButton
+            // 
+            this.desenharButton.BackColor = System.Drawing.Color.LightBlue;
+            this.desenharButton.Location = new System.Drawing.Point(219, 66);
+            this.desenharButton.Name = "desenharButton";
+            this.desenharButton.Size = new System.Drawing.Size(114, 42);
+            this.desenharButton.TabIndex = 3;
+            this.desenharButton.Text = "Desenhar";
+            this.desenharButton.UseVisualStyleBackColor = false;
+            this.desenharButton.Click += new System.EventHandler(this.desenharButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.formula);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.frequenciaTextBox);
+            this.panel1.Controls.Add(this.desenharButton);
+            this.panel1.Controls.Add(this.amplitudeTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 194);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amplitude:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Frequência:";
+            // 
+            // formula
+            // 
+            this.formula.AutoSize = true;
+            this.formula.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formula.Location = new System.Drawing.Point(59, 135);
+            this.formula.Name = "formula";
+            this.formula.Size = new System.Drawing.Size(232, 32);
+            this.formula.TabIndex = 6;
+            this.formula.Text = "f(x) = A * sin(θ * f)";
+            this.formula.Click += new System.EventHandler(this.formula_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(233, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 33);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox amplitudeTextBox;
+        private System.Windows.Forms.TextBox frequenciaTextBox;
+        private System.Windows.Forms.Button desenharButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label formula;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Sin Cos Visualizer/Form1.cs
@@ -0,0 +1,173 @@
+﻿
+/*Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ *Curso Técnico em Informática - Data de Entrega: 05 / 06 / 2024
+ * Autores do Projeto: Isabelly Pacheco Marinho
+ *                     Vitor Lucas Kohls Correa
+ *
+ * Turma: 3F
+ * Atividade Proposta em aula
+ * Observação: < colocar se houver>
+ * 
+ * 
+ * ******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sin_Cos_Visualizer
+{
+    public partial class Form1 : Form
+    {
+        int WIDTH;
+        int HEIGHT;
+        int a = 1;
+        int f = 1;
+        Color cor;
+        //int offset = -25;
+        int offset = 0;
+        bool desenhar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Pen GetPen(int r, int g, int b)
+        {
+            Color color = Color.FromArgb(r, g, b);
+            return new Pen(color);
+        }
+
+        double fun(double x)
+        {
+            if(comboBox1.Text == "Sin")
+            {
+                return a * Math.Sin(x * f * Math.PI/180);
+            }
+            else
+            {
+                return a * Math.Cos(x* f * Math.PI / 180);
+            }
+        }
+
+        float[] CoordenadaDoPlano_CoordenadaDoFormulario(float x, float y)
+        {
+            y *= -1;
+            x += WIDTH / 2;
+            y += HEIGHT / 2;
+
+            return new float[] { x, y };
+        }
+        float[] CoordenadaDoFormulario_CoordenadaDoPlano(float x, float y)
+        {
+            x -= WIDTH / 2;
+            y -= HEIGHT / 2;
+
+            return new float[] { x, y * -1 };
+        }
+
+        void DrawPoint(PaintEventArgs e, Pen pen, float x, float y)
+        {
+            e.Graphics.DrawLine(pen, x, y, x+1, y);
+        }
+
+        void DesenharEixos(PaintEventArgs e)
+        {
+            //Desenha as retas do exio X e do eixo Y
+            Pen pen = GetPen(0, 0, 0);
+
+            float xOrigin = WIDTH / 2;
+            float yOrigin = HEIGHT / 2;
+
+            e.Graphics.DrawLine(pen, 0, yOrigin + offset, WIDTH, yOrigin + offset);//  X
+
+            for (int i = 0; i <= WIDTH; i += 10)
+                DesenharMarcasX(e, pen, i);
+        }
+
+        void DesenharMarcasX(PaintEventArgs e, Pen pen, int x)
+        {
+            float y1 = CoordenadaDoPlano_CoordenadaDoFormulario(0, 1)[1] + offset;
+            float y2 = CoordenadaDoPlano_CoordenadaDoFormulario(0, -1)[1] + offset;
+            e.Graphics.DrawLine(pen, x, y1, x, y2);
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WIDTH = ActiveForm.Size.Width;
+            HEIGHT = ActiveForm.Size.Height;
+            cor = button1.BackColor;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DesenharEixos(e);
+
+            if (!desenhar)
+                return;
+
+            Pen pen = GetPen(cor.R, cor.G, cor.B);
+
+            for (float x = 0; x < WIDTH; x+= 0.1f)
+            {
+                float y = (float)fun(x);
+                float y_form = CoordenadaDoPlano_CoordenadaDoFormulario(0, y)[1];
+                DrawPoint(e, pen, x, y_form);
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void prepareDraw()
+        {
+            desenhar = true;
+            if (amplitudeTextBox.Text != "")
+                a = int.Parse(amplitudeTextBox.Text);
+            else
+                a = 1;
+
+            if (frequenciaTextBox.Text != "")
+                f = int.Parse(frequenciaTextBox.Text);
+            else
+                f = 1;
+
+            amplitudeTextBox.Text = a.ToString();
+            frequenciaTextBox.Text = f.ToString();
+
+            //atualizar a formula:
+            formula.Text = "f(x) = " + a + " * " + comboBox1.Text.ToLower() + "(θ * " + f + ")";
+
+            Invalidate();
+        }
+        private void desenharButton_Click(object sender, EventArgs e)
+        {
+            prepareDraw();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            prepareDraw();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            cor = colorDialog1.Color;
+            button1.BackColor = cor;
+        }
+
+        private void formula_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
