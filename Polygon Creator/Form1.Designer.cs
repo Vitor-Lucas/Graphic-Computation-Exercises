35d7b0216cda61d33996052ac518dc8710d3f14b
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-06-12 10:40:22 -0300

    exercise

---
+++ Polygon Creator/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Polygon_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PaddingChanged += new System.EventHandler(this.Form1_PaddingChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Polygon Creator/Form1.cs
@@ -0,0 +1,74 @@
+﻿/*Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ *Curso Técnico em Informática - Data de Entrega: 12 / 06 / 2024
+ * Autores do Projeto: Isabelly Pacheco Marinho
+ *                     Vitor Lucas Kolhs Correa
+ *
+ * Turma: 2F
+ * Atividade Proposta em aula
+ * Observação: < colocar se houver>
+ * 
+ * 
+ * ******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Polygon_Creator
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        HatchBrush GetHatch(HatchStyle style, Color line, Color fill)
+        {
+            HatchBrush hashura = new HatchBrush(style,line,fill);
+            return hashura;
+        }
+
+        Pen Pen(Color cor)
+        {
+            return new Pen(cor);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_PaddingChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = Pen(Color.Black);
+            HatchBrush hashura = GetHatch(HatchStyle.Vertical, Color.BlueViolet, Color.Magenta);
+           
+            //retangulo
+            e.Graphics.DrawRectangle(pen,10,20,25,50);
+            e.Graphics.FillRectangle(hashura, 10, 20, 25, 50);
+
+            //circulo
+            e.Graphics.DrawEllipse(pen, 100, 200, 50, 50);
+            e.Graphics.FillEllipse(hashura, 100, 200, 50, 50);
+
+            //elipse
+            e.Graphics.DrawEllipse(pen, 150, 250, 50, 30);
+            e.Graphics.FillEllipse(hashura, 150, 250, 50, 30);
+        }
+    }
+}
